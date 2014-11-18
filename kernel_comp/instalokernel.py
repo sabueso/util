b13f0766dae27b944cfed1e1dd2527291b6f34e9
sabueso <sabueso@portatil>
2014-11-18 16:59:31 +0100

    Added arch detection for kernel compile process

--- kernel_comp/instalokernel.py
+++ kernel_comp/instalokernel.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python
 import os,sys
 directorio = sys.argv[1]
+arch_ref={'i686':'i386','x86_64':'amd64'}
 
+#Show linux version
 version=os.popen('make -s -C '+directorio+' kernelversion').read()
+
+#Show arch type for future use
+arch_os=os.popen('arch').read()
+arch_os=arch_os.strip()
+for i in arch_ref:
+	if arch_os in i:
+		directory_arch=str(arch_ref[i])
+
+print('==Compiling '+directory_arch+' kernel version==')
+#Compile ALL
 os.system('make -C '+directorio+' -j12 all')
 os.system('make -C '+directorio+' modules_install')
-os.system('cp '+directorio+'/arch/'+version+'/boot/bzImage /boot/vmlinuz-inestable')
+os.system('cp '+directorio+'/arch/'+directory_arch+'/boot/bzImage /boot/vmlinuz-inestable')
 os.system('rm /boot/initrd.img-inestable')
 os.system('mkinitramfs -v -o /boot/initrd.img-inestable '+version+' ')
 sys.exit()
